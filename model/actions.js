a3cca12df394ebfad87a09a033664f10eeff3518
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-07 13:40:11 +0200

    Create actions

---
+++ model/actions.js
@@ -0,0 +1,21 @@
+export const add = () => {
+    return {
+        type: 'ADD'
+    }
+}
+
+export const subtract = () => {
+    return {
+        type: 'SUBTRACT'
+    }
+}
+
+export const reset = () => {
+    return {
+        type: 'RESET'
+    }
+}
+
+//  console.log(add())
+//  console.log(subtract())
+//  console.log(reset())
